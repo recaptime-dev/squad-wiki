016b50577715d7de8e5b89d09cd2ee95cd1682bd
Andrei Jiroh Halili <ajhalili2006@andreijiroh.eu.org>
2023-08-24 02:53:18 +0000

    ci(netlify): use specialized build script for metadata file
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.eu.org>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 if [[ $FF_DISABLE_VENV_USAGE_DURING_BUILD != "false" ]]; then
+  echo "setting up virtualenv"
   python3 -m venv .venv
   source .venv/bin/activate
+else
+  echo "virtualenv disabled for this build"
 fi
+echo
 
+echo "installing packages"
 pip3 install -r requirements.txt --upgrade
 
+echo "building site"
 mkdocs build --verbose
+echo
 
+echo "copying .well-known files"
 cp -vr content/.well-known site/.well-known
